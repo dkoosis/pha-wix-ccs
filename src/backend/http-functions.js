3b446764eb5acfacf232eb5038762044d418f35f
dkoosis <dkoosis@gmail.com>
2025-06-27 22:30:14 -0400

    change jsw to web.js

--- src/backend/http-functions.js
+++ src/backend/http-functions.js
@@ -1,12 +1,12 @@
 // src/backend/http-functions.js
 // Minimal version with API key - Step 1
-
+// WTF version
 import { ok, serverError, forbidden } from 'wix-http-functions';
 import { getSecret } from 'wix-secrets-backend';
 import { testCollectionAccess } from 'backend/data-access';
 
 const FILLOUT_API_KEY_NAME = "FILLOUT_X_API_KEY";
-const CODE_VERSION = "v.8bf5fd6"; // The script will replace this line
+const CODE_VERSION = "v.3853ef1"; // The script will replace this line
 
 // Test endpoint - with API key validation
 export async function get_ping(request) {
