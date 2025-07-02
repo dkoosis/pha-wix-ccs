2ae4942e84b3e186ef89e541d01dd384915ac98c
teamwixwiz <team@thewixwiz.com>
2025-07-02 14:58:41 +0500

    Update events.js

--- src/backend/events.js
+++ src/backend/events.js
@@ -1,5 +1,5 @@
 import { authorization } from 'wix-members-backend';
-
+import {}
 export async function wixMembers_onMemberCreated(event) {
     const memberId = event.entity._id;
     await authorization.assignRole("2ade445c-7265-420a-8102-484abdd3dc54", memberId, {
@@ -7,3 +7,15 @@ export async function wixMembers_onMemberCreated(event) {
     })
     console.log(`Member with member ID: ${memberId} created.`);
 }
+
+export function wixEcom_onOrderPaymentStatusUpdated(event) {
+  const orderId = event.data.order._id;
+  const currentPaymentStatus = event.data.order.paymentStatus;
+  const previousPaymentStatus = event.data.previousPaymentStatus;
+  const orderTotalPrice = event.data.order.priceSummary.totalPrice.amount;
+  const eventId = event.metadata.id;
+  if(currentPaymentStatus == "PAID") {
+      
+  }
+}
+
